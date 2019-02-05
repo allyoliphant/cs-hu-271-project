b77bb8600b69dd4b0263367e1d6c7f47f9a6c487
benjaminclark964 <46797974+benjaminclark964@users.noreply.github.com>
2019-02-05 12:02:39 -0700

    Tests given from class

--- UserManagerSwing/src/tests/RegisterNewUserTests.java
+++ UserManagerSwing/src/tests/RegisterNewUserTests.java
@@ -1,8 +1,25 @@
 package tests;
 
+import business.UserAccount;
+import business.UserAccountManager;
 import junit.framework.TestCase;
+
 
 public class RegisterNewUserTests extends TestCase{
 	
+	private UserAccountManager userAccountManager;
 	
-}
+	protected void setUp() {
+	userAccountManager = new UserAccountManager();
+	
+	}
+	
+	public void testSuscessfulRegistration(){
+	String registrationResult = userAccountManager.registerNewUser("trump", "@White0House", "@White0House", "Don", "Trump", "trump@gmail.com", "7024265734");
+	assertEquals(registrationResult, UserAccountManager.NOINPUTERROR);
+	UserAccount userAccount = userAccountManager.login("trump", "@White0House"); //valid login
+	assertNotNull(userAccount); //valid account
+	userAccount.getUserName().equalsIgnoreCase("trump"); //valid user name
+	userAccount.getEmail().equalsIgnoreCase("trump@gmail.com"); //valid email
+	}
+	}

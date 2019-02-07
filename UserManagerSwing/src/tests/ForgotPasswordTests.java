3aae768e33a6a31bb909ff47469fcf7d2caa1bac
nvrqu <Benjamin Clark>
2019-02-07 14:16:47 -0700

    Added some tests to forgotUsername and forgotPassword

--- UserManagerSwing/src/tests/ForgotPasswordTests.java
+++ UserManagerSwing/src/tests/ForgotPasswordTests.java
@@ -1,8 +1,34 @@
 package tests;
 
+import business.UserAccount;
+import business.UserAccountManager;
 import junit.framework.TestCase;
 
 public class ForgotPasswordTests extends TestCase{
 	
+	private UserAccountManager uManager;
+	private UserAccount uAccount;
+	private String email = "trump@gmail.com";
 	
+	protected void setUp() {
+		uManager = new UserAccountManager();
+		uAccount = new UserAccount();
+	}
+	
+	public void testValidEmail() {
+	String email = "trump@gmail.com";
+	String registration = uManager.registerNewUser("trump", "@White0House", "@White0House", "Don",
+			"Trump", "trump@gmail.com", "7024265734");
+	
+	//valid email used
+	assertEquals(uManager.forgotUserName(email), uManager.NOINPUTERROR);
+	assertNotNull(uManager.forgotUserName(email));
+}
+
+public void testEmptyEmail() {
+	String invalidEmpty= "";
+	
+	//no email entered
+	assertFalse(uManager.forgotUserName(invalidEmpty).equals(uManager.NOINPUTERROR));
+}
 }
--- UserManagerSwing/src/tests/ForgotUserNameTests.java
+++ UserManagerSwing/src/tests/ForgotUserNameTests.java
@@ -1,8 +1,33 @@
 package tests;
 
+import business.UserAccount;
+import business.UserAccountManager;
 import junit.framework.TestCase;
 
 public class ForgotUserNameTests extends TestCase{
 	
+	private UserAccountManager uManager;
+	private UserAccount uAccount;
 	
+	protected void setUp() {
+		uManager = new UserAccountManager();
+		uAccount = new UserAccount();
+	}
+	
+	public void testValidEmail() {
+		String email = "trump@gmail.com";
+		String registration = uManager.registerNewUser("trump", "@White0House", "@White0House", "Don",
+				"Trump", "trump@gmail.com", "7024265734");
+		
+		//valid email used
+		assertEquals(uManager.forgotUserName(email), uManager.NOINPUTERROR);
+		assertNotNull(uManager.forgotUserName(email));
+	}
+	
+	public void testEmptyEmail() {
+		String invalidEmpty= "";
+		
+		//no email entered
+		assertFalse(uManager.forgotUserName(invalidEmpty).equals(uManager.NOINPUTERROR));
+	}
 }
